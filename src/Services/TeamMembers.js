27088faa1c73674dfd2fafdf2ca868895679c286
samos <samuelguetta@campus.technion.ac.il>
2024-08-29 16:27:03 +0300

    aabnfgllmppp

--- src/Services/TeamMembers.js
+++ src/Services/TeamMembers.js
@@ -1,9 +1,11 @@
 // src/Accueil/TeamMembers.js
 import React from 'react';
 import './TeamMembers.css';
-import member1 from "./../Ressources/photo_dattente.jpg";
-import member2 from "./../Ressources/photo_dattente.jpg"; // remplacez par le chemin réel des images
-import member3 from "./../Ressources/photo_dattente.jpg"; // remplacez par le chemin réel des images
+import member1 from "./../Ressources/amiram_good.jpg";
+import member2 from "./../Ressources/benif_good.jpg"; // remplacez par le chemin réel des images
+import member3 from "./../Ressources/lena_good.jpg";
+import member4 from "./../Ressources/oren_good.jpg";
+import member5 from "./../Ressources/dima_good.jpg"; // remplacez par le chemin réel des images
 import { useTranslation } from 'react-i18next';
 import '../i18n'; // Assurez-vous que ce chemin est correct
 
@@ -33,6 +35,22 @@ const TeamMembers = () => {
       description: t('team_member_3_description_i6'),
       image: member3,
       website: 'https://www.instagram.com/lmh.interior?igsh=MWQ5M3llZDcxeGRiNA%3D%3D&utm_source=qr',
+    },
+    {
+      id: 4,
+      name: t('team_member_4_name_i6'), // Nouveau membre
+      surname: t('team_member_4_surname_i6'),
+      description: t('team_member_4_description_i6'),
+      image: member4,
+      website: '', // Remplacez par le lien du site web
+    },
+    {
+      id: 5,
+      name: t('team_member_5_name_i6'), // Nouveau membre
+      surname: t('team_member_5_surname_i6'),
+      description: t('team_member_5_description_i6'),
+      image: member5,
+      website: '', // Remplacez par le lien du site web
     },
   ];
 
